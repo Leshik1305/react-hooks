3615bf258dde79b09db4f10570951f68aa20f388
Leshik1305 <realleshik1305@gmail.com>
2024-12-18 22:07:24 +0300

    Update TaskThree.jsx

--- homework/homework/src/task-three/TaskThree.jsx
+++ homework/homework/src/task-three/TaskThree.jsx
@@ -1,23 +1,59 @@
-import React, {useState} from 'react';
+import React, {useState, useCallback, useEffect } from 'react';
 import './TaskThree.css';
 
 // функция для получения данных с Mock API
-const fetchData = async (search) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?search=${search}`);
-    return await response.json();
-}
+
+
 
 export default function TaskThree() {
-    const [search, setSearch] = useState();
     const [posts, setPosts] = useState([]);
+    const [search, setSearch] = useState();
+    const [isLoading, setLoading] = useState(true);
+
+    const fetchData = useCallback(async (search, abortController) => {
+        try {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/posts?search=${search}`, { signal: abortController.signal });
+            const data = await res.json();
+            setPosts(data);
+        } catch (error) {
+            if (error.name === 'AbortError') {
+                console.log('Fetch aborted');
+            } else {
+                console.log('An error occurred:', error);
+            }
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        const abortController = new AbortController();
+        setLoading(true);
+        fetchData(search, abortController);
+
+        return () => abortController.abort();
+    }, [search, fetchData]);
+
+    
+
+
 
     return (
         <div className="TaskThree">
-            <input type="text" onChange={(event) => setSearch(event.target.value)} placeholder="Search posts"/>
+            <input type="text" 
+            onChange={(event) => setSearch(event.target.value)} 
+            placeholder="Search posts"/>
             <h1>Posts</h1>
-            <ul>
-                {posts.map(item => <li key={item}>{item}</li>)}
-            </ul>
+            { isLoading
+                ? <div className="loading">Loading...</div>
+                : <ul>
+                {posts.map((item) => (
+                  <li key={item.id}>
+                    {item.title}
+                  </li>
+                ))}
+              </ul> 
+            }
         </div>
     )
 }
